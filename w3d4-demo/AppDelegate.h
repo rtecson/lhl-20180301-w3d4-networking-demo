621bc3ea068d68de37e28e40d3f0db93d813a21d
Roland Tecson <roland_tecson@hotmail.com>
2018-03-01 09:38:22 -0800

    Initial Commit

---
+++ w3d4-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  w3d4-demo
+//
+//  Created by Roland on 2018-03-01.
+//  Copyright © 2018 MoozX Internet Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ w3d4-demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  w3d4-demo
+//
+//  Created by Roland on 2018-03-01.
+//  Copyright © 2018 MoozX Internet Ventures. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
